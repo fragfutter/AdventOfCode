88c86dc6d8e639f0881bf52b10c12ba1008f8925
fragfutter <fragfutter@gmail.com>
2018-12-18 20:29:44 +0100

    CH: 12 part 1

---
+++ 2018/11/11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from advent import Advent
+
+"""
+"""
+
+
+class Day(Advent):
+    lines = True
+
+    def prepare(self):
+        super(Day, self).prepare()
+
+    def solve1(self):
+        pass
+
+    def solve2(self):
+        pass
+
+
+Day.main()
