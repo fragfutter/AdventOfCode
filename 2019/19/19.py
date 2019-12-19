e7fc68f130bee22ed7dbf515f1e8f15d0a546725
fragfutter <fragfutter@gmail.com>
2019-12-19 11:58:53 +0100

    CH: 19

--- 2019/19/19.py
+++ 2019/19/19.py
@@ -20,53 +20,29 @@ class Day(Advent):
     def prepare(self):
         super(Day, self).prepare()
         self.data = self.data[0]
-        self.create_basemap()
-
-    def create_basemap(self):
-        # there is a gap beweeen (0, 0) and the first affected field
-        # iterate the area to (10, 10) to get starting values
-        grid = {}
-        for x in range(10):
-            for y in range(10):
-                grid[Point(x, y)] = runit(self.data, x, y)
-        self.grid = grid
+        self.grid = {}
 
     def create_map(self, limit):
-        # expand grid to cover area up to limit
-        limit = limit - 1
-        grid = self.grid
-        work = [p for p, v in grid.items() if v]
-        while work:
-            p = work.pop(0)
-            grid[p] = runit(self.data, p.x, p.y)
-            if not grid[p]:
-                continue
-            for n in p.neighbours(diagonal=True):
-                if n in work:
-                    continue
-                if n.x < 0 or n.y < 0:
-                    continue
-                if n.x > limit or n.y > limit:
-                    continue
-                if n in grid:
-                    continue
-                work.append(n)
-        self.grid = grid
-        result = sum(grid.values())
-        return result
+        # note that the starting area has rows without affected fields
+        # so we need to trace a few rows completely up to limit
+        y = 0
+        x = 0
+        while not self.get(x, y) and x < limit:
+            x += 1
+        while y < limit:
+            if x == limit:
+                x = 0
+            while not self.get(x, y) and x < limit:
+                x += 1  # find lower left in case we did not have it
+            lx = x  # remember for the next row
+            while self.get(x, y) and x < limit:
+                x += 1  # hit all affected fields
+            y += 1
+            x = lx
 
     def solve1(self):
         self.create_map(50)
         return sum(self.grid.values())
-
-    def sample_grid(self):
-        with open('sample.txt') as fh:
-            data = fh.read()
-        result = {}
-        for y, row in enumerate(data.split('\n')):
-            for x, c in enumerate(row):
-                result[Point(x, y)] = int(c != '.')
-        return result
 
     def dump(self, grid, hl=None):
         xlimit = max([p.x for p in grid])
@@ -89,6 +65,9 @@ class Day(Advent):
             print()
 
     def get(self, x, y):
+        # get grid value and cache it
+        if x < 0 or y < 0:
+            return 0
         p = Point(x, y)
         if p in self.grid:
             return self.grid[p]
@@ -98,25 +77,22 @@ class Day(Advent):
 
     def solve2(self):
         size = 100 - 1
-        # start in the last known row, our grid is to small anyway
-        y = max([p.y for p, v in self.grid.items() if v])
-        x = min([p.x for p, v in self.grid.items() if v and p.y == y])
-        lx = x
+        x = 0
+        y = size  # we check above right, so minimum y is size
+        # find the first affected field
+        while not self.get(x, y):
+            x += 1
         while True:
-            if self.get(x + size, y):
-                if self.get(x, y+size):
-                    break  # found it
-                else:
-                    # but we can go right
-                    x += 1
-                    continue
-            # nothing to the right, new row
+            # check diagonal above right
+            if self.get(x + size, y - size):
+                # got it, get top left corner
+                result = x, y - size
+                break
+            # new row, left corner
             y += 1
-            # find lower left corner
-            while not self.get(lx, y):
-                lx += 1
-            x = lx
-        return x * 10000 + y
+            while not self.get(x, y):
+                x += 1
+        return result[0] * 10000 + result[1]
 
 
 Day.main()
