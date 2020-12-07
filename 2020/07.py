83ba137542da372ea8a583203a8bca42dd14cd28
fragfutter <fragfutter@gmail.com>
2020-12-07 19:40:41 +0100

    CH: 2020-07

--- 2020/07.py
+++ 2020/07.py
@@ -2,7 +2,7 @@
 
 from advent import Advent
 import re
-from collections import deque
+from collections import deque  # we use lists as fifo, this is faster
 """
 """
 
@@ -19,10 +19,10 @@ class Day(Advent):
             assert(start not in result)
             result[start] = targets
         self.data = result
-        # build a reverse graph
+        # build a reverse graph, used in part1
         rev = {}
         for start, targets in self.data.items():
-            for _, target in targets:
+            for _, target in targets:  # not interested in count
                 if target in rev:
                     if start not in rev[target]:
                         rev[target].append(start)
@@ -32,9 +32,9 @@ class Day(Advent):
 
     def solve1(self):
         result = []
-        queue = [self.MY_BAG]
+        queue = deque([self.MY_BAG])
         while queue:
-            bag = queue.pop(0)
+            bag = queue.popleft()
             if bag in result:
                 continue
             result.append(bag)
@@ -43,10 +43,16 @@ class Day(Advent):
             except KeyError:
                 continue
             queue.extend(owners)
-        result.remove(self.MY_BAG)
+        result.remove(self.MY_BAG)  # our bag is not allowed to travel by itself
         return len(result)
 
     def solve2(self):
+        """idea is to find all bags for which we can know the counts
+        avoiding to recalculate the same values over and over,
+        avoiding to hit a recursion limit.
+        In the end we know the count for all bags, as this is Adventofcode
+        i assume that we need to know it anyway and there are no seperate
+        bag-loops"""
         bagcount = {}  # name -> number of bags inside
         queue = deque(self.data.keys())  # all bags
         while queue:
@@ -58,7 +64,8 @@ class Day(Advent):
                     value += count + count * bagcount[contained]
                 bagcount[bag] = value
             except KeyError:
-                # one bag was unknown, insert back into queue
+                # for one bag we did not know how many others it contained
+                # add it back to the queue, we will check it later
                 queue.append(bag)
         return bagcount[self.MY_BAG]
 
