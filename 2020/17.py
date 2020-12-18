f9644ef91ad687590f4716837678881aa1dd8c1a
fragfutter <fragfutter@gmail.com>
2020-12-18 21:08:03 +0100

    CH: 2020-17 working but too slow

--- 2020/17.py
+++ 2020/17.py
@@ -5,14 +5,12 @@ from advent import Advent
 """
 """
 
-class Point3D(object):
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+class Point(object):
+    def __init__(self, *coordinates):
+        self.coordinates = tuple(coordinates)
 
     def as_tuple(self):
-        return (self.x, self.y, self.z)
+        return self.coordinates
 
     def __repr__(self):
         return str(self.as_tuple())
@@ -23,13 +21,37 @@ class Point3D(object):
     def __hash__(self):
         return hash(self.as_tuple())
 
+    def dimensions(self):
+        return len(self.coordinates)
+
+    def neighbours_dimensions(self, dimension):
+        """yield neighbours for dimension"""
+        if dimension >= len(self.coordinates):
+            yield self
+            return  # terminate iteration
+        coordinates = list(self.coordinates)
+        for i in (-1, 0, 1):
+            coordinates[dimension] = i
+            point = Point(*coordinates)
+            for p in point.neighbours_dimensions(dimension + 1):
+                yield p
+
     def neighbours(self):
-        for x in (-1, 0, 1):
-            for y in (-1, 0, 1):
-                for z in (-1, 0, 1):
-                    if x==y==z==0:
-                        continue
-                    yield Point3D(self.x+x, self.y+y, self.z+z)
+        def recursion(dimension):
+            if not dimension:
+                # final
+                yield ()
+                return
+            for outer in recursion(dimension - 1):
+                start = self.coordinates[dimension - 1] - 1
+                stop = self.coordinates[dimension - 1] + 1
+                for inner in range(start, stop + 1):
+                    coordinates = outer + (inner, )
+                    yield coordinates
+
+        for coordinates in recursion(self.dimensions()):
+            if coordinates != self.coordinates:
+                yield Point(*coordinates)
 
 
 class Grid(object):
@@ -49,19 +71,23 @@ class Grid(object):
             pass
 
     def iterate(self, offset=1):
-        for x in range(
-            min(self.data, key=lambda i: i.x).x - offset,
-            max(self.data, key=lambda i: i.x).x + offset + 1):
-            for y in range(
-                min(self.data, key=lambda i: i.y).y - offset,
-                max(self.data, key=lambda i: i.y).y + offset + 1):
-                for z in range(
-                    min(self.data, key=lambda i: i.z).z - offset,
-                    max(self.data, key=lambda i: i.z).z + offset + 1):
+        def recursion(dimension):
+            if not dimension:
+                # final
+                yield ()
+                return
+            for outer in recursion(dimension - 1):
+                start = min(map(lambda point: point.coordinates[dimension - 1], self.data)) - offset
+                stop  = max(map(lambda point: point.coordinates[dimension - 1], self.data)) + offset
+                for inner in range(start, stop + 1):
+                    coordinates = outer + (inner, )
+                    yield coordinates
 
-                    yield Point3D(x, y, z)
+        dimension = self.data[0].dimensions()
+        for coordinates in recursion(dimension):
+            yield Point(*coordinates)
 
-    def count(self, point):
+    def count_neighbours(self, point):
         result = 0
         for n in point.neighbours():
             if n in self.data:
@@ -71,33 +97,47 @@ class Grid(object):
 
 class Day(Advent):
     matrix = True
-    conversion = lambda x: x=='#'
+    # conversion = lambda x: x=='#'
 
     def prepare(self):
-        result = Grid()
+        result = []
         for y, line in enumerate(self.data.split('\n')):
             for x, c in enumerate(line.strip()):
                 if c == '#':
-                    result.activate(Point3D(x, y, 0))
+                    result.append(Point(x, y))
         self.data = result
 
-    def solve1(self):
-        result = self.data
+    def solve_(self, dimensions):
+        extension = (0, ) * (dimensions - 2)
+        result = Grid()
+        for point in self.data:
+            coordinates = point.coordinates + extension
+            result.activate(Point(*coordinates))
         for i in range(6):
             new = Grid()
             for point in result.iterate():
-                count = result.count(point)
+                count = result.count_neighbours(point)
                 if point in result.data:
+                    # if an activate point has 2 or 3 neighbours
+                    # it stays active
                     if count in (2, 3):
                         new.activate(point)
+                    # otherwise it becomes inactive
                 else:
+                    # if an inactivate point has 3 neighbours
+                    # it becomes active
                     if count == 3:
                         new.activate(point)
+                    # otherwiese it stays inactive
             result = new
         return len(result.data)
 
+    def solve1(self):
+        return self.solve_(3)
+
     def solve2(self):
-        pass
+        import profile
+        profile.runctx('self.solve_(3)', globals(), locals())
 
 
 Day.main()
