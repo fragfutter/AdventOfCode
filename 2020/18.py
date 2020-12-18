44d3c00da2c2a46b8238acc1bd0f8b9be4aeb543
fragfutter <fragfutter@gmail.com>
2020-12-18 23:14:03 +0100

    CH: 2020-18 both parts as regexp

--- 2020/18.py
+++ 2020/18.py
@@ -4,103 +4,88 @@ from advent import Advent
 import re
 
 """
+solution 1: 21993583522852
+solution 2: 122438593522757
 """
-ADD = '+'
-MUL = '*'
-NOP = None
+class Calculator(object):
+    pat_bracket = re.compile(r'\(([\d*+ ]+)\)')
+    pat_op = re.compile(r'(\d+)\s*([+*])\s*(\d+)')
+    pat_add = re.compile(r'(\d+)\s*\+\s*(\d+)')
+    pat_mul = re.compile(r'(\d+)\s*\*\s*(\d+)')
 
-def multiply(m):
-    result = int(m.group(1)) * int(m.group(2))
-    return str(result)
+    def __init__(self, part_two=False ):
+        """
+        find pattern we can solve and replace it with result
+        repeat until nothing changes, then resolve next operator
 
-def add(m):
-    result = int(m.group(1)) + int(m.group(2))
-    return str(result)
+        for part1, first search for anything that is in brackets
+        and solve that sub-problem
 
-def bracket(m):
-    return calc(m.group(1))
+        then resolve from left + and *
 
-def calc(data):
-    current = data
-    # brackets
-    while True:
-        reduced = re.sub(r'\(([\d*+ ]+)\)', bracket, current, count=1)
-        if reduced == current:
-            break
-        current = reduced
-    # add
-    while True:
-        reduced = re.sub(r'(\d+)\s*\+\s*(\d+)', add, current, count=1)
-        if reduced == current:
-            break
-        current = reduced
-    # multiply
-    while True:
-        reduced = re.sub(r'(\d+)\s*\*\s*(\d+)', multiply, current, count=1)
-        if reduced == current:
-            break
-        current = reduced
-    return current
+        for part2, brackets stay the same.
+        then resolve all +
+        and finally all *
+        """
+        if not part_two:
+            self.tasks = (
+                (self.pat_bracket, self.bracket),
+                (self.pat_op, self.op),
+            )
+        else:
+            self.tasks = (
+                (self.pat_bracket, self.bracket),
+                (self.pat_add, self.add),
+                (self.pat_mul, self.multiply),
+            )
+
+    def bracket(self, m):
+        return self.calc(m.group(1))
+
+    def op(self, m):
+        a, o, b = m.groups()
+        a = int(a)
+        b = int(b)
+        if o == '+':
+            result = a + b
+        else:
+            result = a * b
+        return str(result)
+
+    def multiply(self, m):
+        result = int(m.group(1)) * int(m.group(2))
+        return str(result)
+
+    def add(self, m):
+        result = int(m.group(1)) + int(m.group(2))
+        return str(result)
+
+    def calc(self, data):
+        current = data
+        for search, replace in self.tasks:
+            while True:
+                reduced = search.sub(replace, current, count=1)
+                if reduced == current:
+                    break
+                current = reduced
+        return current
 
 
 class Day(Advent):
     lines = True
 
-    def prepare1(self):
-        result = []
-        for line in self.data:
-            # assume all single digits
-            stack = []
-            ops = []
-            for token in line:
-                if token == ' ':
-                    continue
-                if token in (ADD, MUL):
-                    ops.append(token)
-                elif token == '(':
-                    ops.append(NOP)
-                elif token == ')':
-                    try:
-                        op = ops.pop()
-                        if op != NOP:
-                            stack.append(op)
-                    except IndexError:
-                        pass
-                else:
-                    stack.append(int(token))
-                    try:
-                        op = ops.pop()
-                        if op != NOP:
-                            stack.append(op)
-                    except IndexError:
-                        pass
-            assert(len(ops) == 0)
-            result.append(stack)
-        return result
-
     def solve1(self):
+        calculator = Calculator()
         result = 0
-        for calc in self.prepare1():
-            stack = []
-            for token in calc:
-                if token == ADD:
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(a + b)
-                elif token == MUL:
-                    a = stack.pop()
-                    b = stack.pop()
-                    stack.append(a * b)
-                else:
-                    stack.append(token)
-            assert(len(stack) == 1)
-            result += stack[0]
+        for line in self.data:
+            result += int(calculator.calc(line))
         return result
 
     def solve2(self):
+        calculator = Calculator(part_two=True)
         result = 0
         for line in self.data:
-            result += int(calc(line))
+            result += int(calculator.calc(line))
         return result
 
 
